72a47c37dfae37557812ba376818758e4364a3d5
neilstottler <2000nesman@gmail.com>
2018-11-07 10:06:38 -0500

    More Buttons and a Chat

--- src/GUIExample.java
+++ src/GUIExample.java
@@ -8,26 +8,26 @@ import javax.swing.JTextArea;
 
 public class GUIExample {
 
-	  public static void main(String[] args) {
+	public static void main(String[] args) {
 
-		    JFrame f = new JFrame("A JFrame");
-		    f.setSize(250, 250);
-		    f.setLocation(300,200);
-		    final JTextArea textArea = new JTextArea(10, 40);
-		    f.getContentPane().add(BorderLayout.CENTER, textArea);
-		    final JButton button = new JButton("Click Me");
-		    f.getContentPane().add(BorderLayout.SOUTH, button);
-		    button.addActionListener(new ActionListener() {
+		JFrame f = new JFrame("A JFrame");
+		f.setSize(250, 250);
+		f.setLocation(300,200);
+		final JTextArea textArea = new JTextArea(10, 40);
+		f.getContentPane().add(BorderLayout.CENTER, textArea);
+		final JButton button = new JButton("Click Me");
+		f.getContentPane().add(BorderLayout.SOUTH, button);
+		button.addActionListener(new ActionListener() {
 
-		        @Override
-		        public void actionPerformed(ActionEvent e) {
-		            textArea.append("Button was clicked\n");
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				textArea.append("Button was clicked\n");
 
-		        }
-		    });
+			}
+		});
 
-		    f.setVisible(true);
+		f.setVisible(true);
 
-		  }
+	}
 
-		}
+}
