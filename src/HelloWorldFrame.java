e1cd85f170bfa050250e915e248620a0fe96efa7
neilstottler <2000nesman@gmail.com>
2018-11-07 10:30:02 -0500

    simple windows with hello world

---
+++ src/HelloWorldFrame.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+//http://www.wideskills.com/java-tutorial/java-swing-tutorial
+
+public class HelloWorldFrame extends JFrame {
+
+	public static void main(String args[]) {
+		new HelloWorldFrame();
+	}
+	HelloWorldFrame() {
+		JLabel jlbHelloWorld = new JLabel("Hello World");
+		add(jlbHelloWorld);
+		this.setSize(200, 200);
+		// pack();
+		setVisible(true);
+	}
+}
